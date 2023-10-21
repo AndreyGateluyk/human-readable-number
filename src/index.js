8caf78d8209de0d6135a06fa219df0e3086bdc33
Andrey Gateluyk <gatelyuk97@mail.ru>
2023-10-21 17:24:25 +0300

    comptite task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-module.exports = function toReadable (number) {
-  
+const upToTwenty = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten',
+'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+
+
+module.exports = function toReadable(number) {
+
+  let string = ''
+
+  if (number < 20) {
+    string += upToTwenty[number]
+  } 
+  else if (number < 100) {
+    string += tens[~~(number / 10)] + (number % 10 > 0 ? ' ' + toReadable(number % 10) : '')
+  } 
+  else if (number < 1000) {
+    string += upToTwenty[~~(number / 100)] + ' hundred' + (number % 100 > 0 ? ' ' + toReadable(number % 100) : '')
+  }
+  return string
 }
